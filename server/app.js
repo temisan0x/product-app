d84daa8cb5dab243a9a9dff18e153df6e5657c3f
temycodes <temycodes@gmail.com>
2025-06-14 16:33:30 +0100

    build: code cleanup

--- server/app.js
+++ server/app.js
@@ -10,13 +10,6 @@ const globalErrorHandler = require('./controllers/errorContoller');
 const app = express();
 const PORT = process.env.PORT || "4000";
 
-app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:3001'], 
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -62,6 +55,13 @@ app.use((req, res) => {
 
 
 app.use(globalErrorHandler);
+
+app.use(cors({
+  origin: ['http://localhost:3000', 'http://localhost:3001'], 
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 app
   .listen(PORT, () => {
